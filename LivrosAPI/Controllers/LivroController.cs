c804611846cb4006b804fb2b4c589d1f0d4b21de
Leonardo Lima <leonardolimaf54@gmail.com>
2022-07-09 15:32:13 -0300

    implementação inicial

---
+++ LivrosAPI/Controllers/LivroController.cs
@@ -0,0 +1,37 @@
+﻿using LivrosAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LivrosAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LivroController : ControllerBase
+{
+    public static List<Livro?> Livros { get; set; } = new List<Livro?>();
+    private static int id = 1;
+    
+    [HttpPost]
+    public IActionResult AdicionarLivro([FromBody] Livro? livro)
+    {
+        livro.Id = id++;
+        Livros.Add(livro);
+        return CreatedAtAction(nameof(AdicionarLivro), new {Id = livro.Id}, livro);
+    }
+
+    [HttpGet]
+    public IActionResult ListarLivros()
+    {
+        return Ok(Livros);
+    }
+    
+    [HttpGet("{id:int}")]
+    public IActionResult ObterLivroPorId(int id)
+    {
+        var livro =  Livros.FirstOrDefault(l => l.Id == id);
+        if (livro != null)
+        {
+            Ok(livro);
+        }
+        return NotFound(livro);
+    }
+}
---
+++ LivrosAPI/Models/Livro.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LivrosAPI.Models;
+
+public class Livro
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+    public string Titulo { get; set; }
+    public string Subtitulo { get; set; }
+    [StringLength(10, MinimumLength = 10, ErrorMessage = "O campo {0} só pode ter {2} caracteres.")]
+    public string ISBN10 { get; set; }
+    [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+    [StringLength(13, MinimumLength = 13, ErrorMessage = "O campo {0} só pode ter {2} caracteres.")]
+    public string ISBN13 { get; set; }
+    public string Serie { get; set; }
+    public int Volume { get; set; }
+    public List<string> Generos { get; set; }
+    public int QuantidadeDePaginas { get; set; }
+    [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+    public string Autor { get; set; }
+    public string Editora { get; set; }
+}
