83032243f512f2b8d2f481dd4af2e8b289c83e4f
Leonardo Lima <leolimaf@outlook.com>
2022-08-13 15:59:17 -0300

    Implementação UsuarioService

--- LivrosAPI/Services/Implementations/UsuarioService.cs
+++ LivrosAPI/Services/Implementations/UsuarioService.cs
@@ -1,21 +1,43 @@
-﻿using LivrosAPI.Data.DTOs.Usuario;
+﻿using AutoMapper;
+using LivrosAPI.Data;
+using LivrosAPI.Data.DTOs.Usuario;
+using LivrosAPI.Models;
 
 namespace LivrosAPI.Services.Implementations;
 
 public class UsuarioService : IUsuarioService
 {
+    private AppDbContext _context;
+    private IMapper _mapper;
+
+    public UsuarioService(AppDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
     public LerUsuarioDTO CadastrarUsuario(AdicionarUsuarioDTO usuarioDto)
     {
-        throw new NotImplementedException();
+        Usuario usuario = _mapper.Map<Usuario>(usuarioDto);
+        _context.Usuarios.Add(usuario);
+        _context.SaveChanges();
+        return _mapper.Map<LerUsuarioDTO>(usuario);
     }
 
     public List<LerUsuarioDTO> ListarUsuarios()
     {
-        throw new NotImplementedException();
+        List<Usuario> usuarios = _context.Usuarios.ToList();
+        return _mapper.Map<List<LerUsuarioDTO>>(usuarios);
     }
 
     public LerUsuarioDTO ObterUsuarioPorId(long id)
     {
-        throw new NotImplementedException();
+        Usuario usuario = _context.Usuarios.FirstOrDefault(usuario => usuario.Id == id);
+        if (usuario is not null)
+        {
+            LerUsuarioDTO usuarioDto = _mapper.Map<LerUsuarioDTO>(usuario);
+            return usuarioDto;
+        }
+        return null;
     }
 }
