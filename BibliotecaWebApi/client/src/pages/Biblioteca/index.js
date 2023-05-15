51ea7cdf5edfae85d8a7687f61d46f75cfc6654f
Leonardo Lima <leonardolimaf54@gmail.com>
2023-05-15 00:39:37 -0300

    front-end: listagem dos livros

--- BibliotecaWebApi/client/src/pages/Biblioteca/index.js
+++ BibliotecaWebApi/client/src/pages/Biblioteca/index.js
@@ -1,16 +1,35 @@
 import './styles.css';
 import logoImage from '../../assets/logo.svg'
-
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { FiPower, FiEdit, FiTrash2 } from 'react-icons/fi'
+import api from "../../services/api";
 
 export default function Livro(){
+
+    const [livros, setLivros] = useState([]);
+
+    const matricula = localStorage.getItem('matricula');
+    const accessToken = localStorage.getItem('accessToken');
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        api.get('v1/biblioteca/listar-livros', {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }).then(response => {
+            setLivros(response.data)
+        })
+    }, [accessToken])
+
     return(
         <div className="book-container">
             <header>
                 <img src={logoImage} alt="Erudio"/>
-                <span>Bem vindo, <strong>Leonardo</strong>!</span>
+                <span>Bem vindo, <strong>{matricula.toLowerCase()}</strong>!</span>
                 <Link className="button" to="adicionar-livro">Adicionar Livro</Link>
                 <button type="button">
                     <FiPower size={18} color="#251fc5" />
@@ -19,132 +38,29 @@ export default function Livro(){
 
             <h1>Livros Registrados</h1>
             <ul>
-                <li>
-                    <strong>Título:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Autor:</strong>
-                    <p>DNigel Poulton</p>
-                    <strong>Data de Lançamento:</strong>
-                    <p>12/07/2017</p>
-                    <strong>ISBN-13</strong>
-                    <p>1234567890123</p>
-                    <strong>Série</strong>
-                    <p>Docker 123</p>
-                    <strong>Volume</strong>
-                    <p>1</p>
+                {livros.map(livro => (
+                    <li key={livro.id}>
+                        <strong>Título:</strong>
+                        <p>{livro.titulo}</p>
+                        <strong>Autor:</strong>
+                        <p>{livro.autor.nomeCompleto}</p>
+                        <strong>Ano de Lançamento:</strong>
+                        <p>{livro.anoPublicacao}</p>
+                        <strong>ISBN-13</strong>
+                        <p>{livro['isbn-13']}</p>
+                        <strong>Série</strong>
+                        <p>{livro.serie}</p>
+                        <strong>Volume</strong>
+                        <p>{livro.volume}</p>
 
-                    <button type="button">
-                        <FiEdit size={20} color="#251fc5"/>
-                    </button>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251fc5"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>Título:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Autor:</strong>
-                    <p>DNigel Poulton</p>
-                    <strong>Data de Lançamento:</strong>
-                    <p>12/07/2017</p>
-                    <strong>ISBN-13</strong>
-                    <p>1234567890123</p>
-                    <strong>Série</strong>
-                    <p>Docker 123</p>
-                    <strong>Volume</strong>
-                    <p>1</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251fc5"/>
-                    </button>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251fc5"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>Título:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Autor:</strong>
-                    <p>DNigel Poulton</p>
-                    <strong>Data de Lançamento:</strong>
-                    <p>12/07/2017</p>
-                    <strong>ISBN-13</strong>
-                    <p>1234567890123</p>
-                    <strong>Série</strong>
-                    <p>Docker 123</p>
-                    <strong>Volume</strong>
-                    <p>1</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251fc5"/>
-                    </button>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251fc5"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>Título:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Autor:</strong>
-                    <p>DNigel Poulton</p>
-                    <strong>Data de Lançamento:</strong>
-                    <p>12/07/2017</p>
-                    <strong>ISBN-13</strong>
-                    <p>1234567890123</p>
-                    <strong>Série</strong>
-                    <p>Docker 123</p>
-                    <strong>Volume</strong>
-                    <p>1</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251fc5"/>
-                    </button>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251fc5"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>Título:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Autor:</strong>
-                    <p>DNigel Poulton</p>
-                    <strong>Data de Lançamento:</strong>
-                    <p>12/07/2017</p>
-                    <strong>ISBN-13</strong>
-                    <p>1234567890123</p>
-                    <strong>Série</strong>
-                    <p>Docker 123</p>
-                    <strong>Volume</strong>
-                    <p>1</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251fc5"/>
-                    </button>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251fc5"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>Título:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Autor:</strong>
-                    <p>DNigel Poulton</p>
-                    <strong>Data de Lançamento:</strong>
-                    <p>12/07/2017</p>
-                    <strong>ISBN-13</strong>
-                    <p>1234567890123</p>
-                    <strong>Série</strong>
-                    <p>Docker 123</p>
-                    <strong>Volume</strong>
-                    <p>1</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251fc5"/>
-                    </button>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251fc5"/>
-                    </button>
-                </li>
+                        <button type="button">
+                            <FiEdit size={20} color="#251fc5"/>
+                        </button>
+                        <button type="button">
+                            <FiTrash2 size={20} color="#251fc5"/>
+                        </button>
+                    </li>
+                ))}
             </ul>
         </div>
     );
